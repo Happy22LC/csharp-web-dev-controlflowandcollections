3af4e764fe364dc4fe7079e49c1bb3b301546675
Happy <biswasnedhi09@gmail.com>
2022-08-09 19:28:12 -0500

    completed the dictionary

--- DictionaryExercises/Program.cs
+++ DictionaryExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DictionaryExercises
 {
@@ -7,6 +8,37 @@ namespace DictionaryExercises
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //It takes in student names and ID numbers (as integers) instead of names and grades.
+            //The keys should be the IDs and the values should be the names.
+            Dictionary<int, string> students = new Dictionary<int, string>();
+            string newStudent;
+
+            Console.WriteLine("Enter your student name and ID numbers (or ENTER to finish)");
+            do
+            {
+                Console.WriteLine("Name: ");
+                string input = Console.ReadLine();
+                newStudent = input;
+
+                if (newStudent != "")
+                {
+                    Console.WriteLine("ID number: ");
+                    input = Console.ReadLine();
+                    int newID = int.Parse(input);
+                    students.Add(newID, newStudent);
+                }
+            } while (newStudent != "");
+
+            //Modify the roster printing code accordingly.
+            Console.WriteLine("\nClass Roster");
+
+            foreach (KeyValuePair<int, string> student in students)
+            {
+                Console.WriteLine($"{student.Key} :{student.Value}");
+            }
+
+
         }
     }
 }
