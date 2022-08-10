138a40bce0420570bb5afb8e35c2e0c75ceb3c89
Happy <biswasnedhi09@gmail.com>
2022-08-09 19:33:32 -0500

    completed the string exercise

--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -7,6 +7,19 @@ namespace StringExercises
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Strings!");
+
+            //create a string
+            string sentence = @"I would not, could not, in a box. I would not, could not with a fox.I will not eat them in a house. I will not eat them with a mouse.";
+
+            //split at spaces to create array
+            string[] sentenceArray = sentence.Split(" ");
+
+            //print array
+            Console.WriteLine(string.Join(",", sentenceArray));
+
+            //repeat to separate the sentence
+            string[] sentenceArray2 = sentence.Split(" ");
+            Console.WriteLine(string.Join("/", sentenceArray2));
         }
     }
 }
