01a291ec71cc0294b679f97129e9b0357a463768
Happy <biswasnedhi09@gmail.com>
2022-08-09 19:25:55 -0500

    completed

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -7,6 +7,25 @@ namespace ArrayExercises
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Arrays!");
+
+            //initialize array in a single line
+            int[] numArray = { 1, 1, 2, 3, 5, 8 };
+
+            //Loop through the array and print each value.
+            foreach (int num in numArray)
+            {
+                Console.WriteLine(num);
+            }
+
+            //Modify t0 print only the odd numbers
+            foreach (int num in numArray)
+            {
+                if (num % 2 == 1)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+
         }
     }
 }
